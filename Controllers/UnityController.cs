cb9b77c377285ebf87eea74cf99f14492f86570d
ElizabethLop <elizabeth_lopez1@usmp.pe>
2020-07-08 09:46:47 -0500

    master

---
+++ Controllers/UnityController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LaboratorioVirtual.Models;
+
+namespace LaboratorioVirtual.Controllers
+{
+    public class UnityController:Controller{
+
+        public IActionResult Index(){
+            return View();
+        }
+
+
+    }
+}
